34061c6637ccaac88026284f485384a6675e1e12
Fabricio <fabribanda438@gmail.com>
2025-09-01 21:25:02 -0600

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Fabricio Banda on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubTestTests/GitHubTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestTests.swift
+//  GitHubTestTests
+//
+//  Created by Fabricio Banda on 01/09/25.
+//
+
+import Testing
+@testable import GitHubTest
+
+struct GitHubTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
